50eab821cbadf0719fca6826f9ad7e01497f70f8
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2023-05-01 23:38:54 +0200

    Add homepage

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 import StopwatchView from "./features/stopwatch-view"
+import Homepage from "./Homepage"
 import './App.css';
 
 function App() {
   return (
     <Routes>
-        <Route path='/' element={<StopwatchView/>}/>
+        <Route path='/' element={<Homepage/>}/>
         <Route path='/stopwatch' element={<StopwatchView/>}/>
         <Route path='/timer' element={<StopwatchView/>}/>
         <Route path="*" element={<Navigate to="/"/>}/> 
---
+++ frontend-react/src/Homepage.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export default function Homepage() { 
+    return (
+        <>
+            <a href="/stopwatch">Stopwatch</a>
+            <br/>
+            <a href="/timer">Timer</a>
+            <br/>
+            <a href="/summary">Summary</a>
+        </>
+    );    
+}
+
