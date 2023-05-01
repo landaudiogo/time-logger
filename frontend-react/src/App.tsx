a8c96a3812ae84314550bcec97fc2893cbc8df81
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2023-05-01 23:45:04 +0200

    Add first timer-view support

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 import StopwatchView from "./features/stopwatch-view"
+import TimerView from "./features/timer-view"
 import Homepage from "./Homepage"
 import './App.css';
 
@@ -9,7 +10,7 @@ function App() {
     <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='/stopwatch' element={<StopwatchView/>}/>
-        <Route path='/timer' element={<StopwatchView/>}/>
+        <Route path='/timer' element={<TimerView/>}/>
         <Route path="*" element={<Navigate to="/"/>}/> 
     </Routes>
   );
---
+++ frontend-react/src/features/timer-view/index.tsx
@@ -0,0 +1,3 @@
+import TimerView from "./components/TimerView"
+
+export default TimerView;
