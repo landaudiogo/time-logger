3ecf1f1da10b8c4cea8ec9c1d0ef72a0f822e1c9
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2023-05-11 16:20:28 +0200

    Add docker container to serve build

--- frontend-react/run.sh
+++ frontend-react/run.sh
@@ -4,8 +4,6 @@ docker build -t time-logger/frontend .
 
 docker run \
     -it --rm \
-    -v "$(pwd)/node_modules:/app/node_modules" \
-    -v "$(pwd)/src:/app/src" \
     --name time-logger-frontend \
-    -p 3000:3000 \
+    -p 80:3000 \
     time-logger/frontend
