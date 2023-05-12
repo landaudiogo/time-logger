d49c41b789539c72a42e796445759886f913d03d
Diogo Landau <diogo.hewitt.landau@hotmail.com>
2023-05-12 19:38:59 +0200

    Add summary to daily records

--- frontend-react/src/features/stopwatch-view/components/DailyRecords.tsx
+++ frontend-react/src/features/stopwatch-view/components/DailyRecords.tsx
@@ -4,20 +4,40 @@ import { prettyPrintTimestamp } from "../lib/dateParsing"
 import { selectRecords } from "../store";
 import { useSelector } from "react-redux";
 
+
 export default function DailyRecords() {
     const stateRecords = useSelector(selectRecords);
 
+    const totalTime = Object.values(stateRecords.records).reduce<Record<string, number>>(
+        (accumulator, currentValue) => {
+            var val = 0;
+            if (accumulator[currentValue.tag] !== undefined)
+                val = accumulator[currentValue.tag];
+            accumulator[currentValue.tag] = val + (currentValue.endTime - currentValue.startTime);
+            return accumulator;
+        }, 
+        {}
+    );
+    const dailyRecords = Object.values(stateRecords.records).map((lapRecord) => {
+        var startDatetimeString = prettyPrintTimestamp(lapRecord.startTime);
+        var endDatetimeString = prettyPrintTimestamp(lapRecord.endTime);
+        return (
+            <p key={lapRecord.lap}>
+                {startDatetimeString} | {endDatetimeString} | {lapRecord.tag}
+            </p>
+        );
+    })
+
+    const summary = Object.entries(totalTime).map(([tag, totalMs]) => (
+        <p key={tag}>
+            {tag} | {prettyPrintTimestamp(totalMs)}
+        </p>
+    ));
+
     return (
         <div>
-            {Object.values(stateRecords.records).map((lapRecord) => {
-                var startDatetimeString = prettyPrintTimestamp(lapRecord.startTime);
-                var endDatetimeString = prettyPrintTimestamp(lapRecord.endTime);
-                return (
-                    <p key={lapRecord.lap}>
-                        {startDatetimeString} | {endDatetimeString} | {lapRecord.tag}
-                    </p>
-                );
-            })}
+            {dailyRecords}
+            {summary}
         </div>
     );
 }
